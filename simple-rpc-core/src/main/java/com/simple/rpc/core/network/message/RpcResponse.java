e2e810b3f1db69fe96413acb38f39c67554d8762
wuchengxing <1449697757@qq.com>
2022-04-26 22:11:30 +0800

    feat：考虑新增自定义编解码协议

---
+++ simple-rpc-core/src/main/java/com/simple/rpc/core/network/message/RpcResponse.java
@@ -0,0 +1,12 @@
+package com.simple.rpc.core.network.message;
+
+/**
+ * 项目: simple-rpc
+ * <p>
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2022-04-26 22:10
+ **/
+public class RpcResponse {
+}
