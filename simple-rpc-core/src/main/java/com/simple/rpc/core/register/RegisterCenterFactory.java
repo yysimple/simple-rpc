9c3a86d29b14fa34b7a86be48a83a410b0dee141
wuchengxing <1449697757@qq.com>
2022-05-04 14:47:01 +0800

    feat：local注册中心暂不使用

--- simple-rpc-core/src/main/java/com/simple/rpc/core/register/RegisterCenterFactory.java
+++ simple-rpc-core/src/main/java/com/simple/rpc/core/register/RegisterCenterFactory.java
@@ -5,6 +5,10 @@ import com.simple.rpc.core.register.strategy.LocalRegisterCenter;
 import com.simple.rpc.core.register.strategy.MysqlRegisterCenter;
 import com.simple.rpc.core.register.strategy.RedisRegisterCenter;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * 项目: simple-rpc
  * <p>
@@ -15,14 +19,22 @@ import com.simple.rpc.core.register.strategy.RedisRegisterCenter;
  **/
 public class RegisterCenterFactory {
 
+    static Map<String, RegisterCenter> REGISTER_CENTER_CACHE = new ConcurrentHashMap<>(16);
+
     public static RegisterCenter create(String registerType) {
-        if (RegisterEnum.LOCAL.getRegisterType().equals(registerType)) {
-            return new LocalRegisterCenter();
-        } else if (RegisterEnum.REDIS.getRegisterType().equals(registerType)) {
-            return new RedisRegisterCenter();
-        } else if (RegisterEnum.MYSQL.getRegisterType().equals(registerType)) {
-            return new MysqlRegisterCenter();
+        return getRegisterCenter(registerType);
+    }
+
+    private static RegisterCenter getRegisterCenter(String registerType) {
+        if (Objects.isNull(REGISTER_CENTER_CACHE.get(registerType))) {
+            if (RegisterEnum.LOCAL.getRegisterType().equals(registerType)) {
+                REGISTER_CENTER_CACHE.put(registerType, new LocalRegisterCenter());
+            } else if (RegisterEnum.REDIS.getRegisterType().equals(registerType)) {
+                REGISTER_CENTER_CACHE.put(registerType, new RedisRegisterCenter());
+            } else if (RegisterEnum.MYSQL.getRegisterType().equals(registerType)) {
+                REGISTER_CENTER_CACHE.put(registerType, new MysqlRegisterCenter());
+            }
         }
-        return null;
+        return REGISTER_CENTER_CACHE.get(registerType);
     }
 }
